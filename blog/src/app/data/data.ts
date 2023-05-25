e5b9e2472ee54ec82c842a427318818adc8be034
Cjr88 <carloscjr70@gmail.com>
2023-05-24 21:07:37 -0300

    content created with success

---
+++ blog/src/app/data/data.ts
@@ -0,0 +1,14 @@
+export const dataFake = [
+  {
+    "id":'1',
+    "title": "News",
+    "description": "The news is just about innovation",
+    "photoCover": "https://www.tsnn.com/sites/default/files/technology1.jpg"
+  },
+  {
+    "id":'2',
+    "title": "Busquem conhecimento",
+    "description": "Eterno ET BILU",
+    "photoCover": "https://i0.wp.com/blog.portaleducacao.com.br/wp-content/uploads/2022/02/365-O-que-e%CC%81-tecnologia_.jpg"
+  },
+]
